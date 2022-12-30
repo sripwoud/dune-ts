b795ffe658fa8199a750e6d6e3ab297d4aa83e6a
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-30 21:59:27 +0100

    fix config file for tests

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,16 @@ import { readFileSync } from 'fs'
 import yaml from 'js-yaml'
 import { join } from 'path'
 
+const isTest = process.env.NODE_ENV === 'test'
+const configFile = isTest ? 'config.sample.yaml' : '.config.yaml'
+
 export interface Config {
   password?: string
   username?: string
 }
 
 const config = yaml.load(
-  readFileSync(join(__dirname, '..', '.config.yaml'), 'utf8'),
+  readFileSync(join(__dirname, '..', configFile), 'utf8'),
 ) as Config
 
 config.password ??= process.env.DUNE_PWD
