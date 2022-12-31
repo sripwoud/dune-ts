a8f464ac98f1008450195b582697b73887536830
3pwd <3pwd@tuta.io>
2022-12-31 11:29:15 +0100

    ✨ Define `login` public method (#4)

--- test/unit/Dune.test.ts
+++ test/unit/Dune.test.ts
@@ -1,6 +1,6 @@
 import { Dune } from 'src'
-import { COOKIES } from './fixtures'
-import { voidFn } from './utils'
+import { URLS } from 'src/constants'
+import { COOKIES, CSRF_TOKEN, TOKEN } from './fixtures'
 
 let dune: Dune
 
@@ -32,13 +32,13 @@ describe('Dune', () => {
 
   describe('getCsrfToken', () => {
     it('sets csrf cookie', async () => {
-      const CSRF_TOKEN = '1234'
       fetchMock.once('', { headers: { 'set-cookie': `csrf=${CSRF_TOKEN}` } })
 
       // using array notation to access private methods
       await dune['getCsrfToken']()
 
       expect(dune.csrf).toEqual(CSRF_TOKEN)
+      expect(fetchMock).toHaveBeenCalledOnceWith(URLS.CSRF)
     })
 
     it('throws error if csrf token is not found', async () => {
@@ -60,36 +60,55 @@ describe('Dune', () => {
         },
       })
 
-      jest
-        .spyOn(Dune.prototype as any, 'getCsrfToken')
-        .mockImplementationOnce(voidFn)
-
       await dune['getAuthCookies']()
 
-      expect(Dune.prototype['getCsrfToken']).toHaveBeenCalledOnce()
       Object.entries(COOKIES).forEach(([cookieName, cookieValue]) => {
         expect(dune['cookies'].getCookie(cookieName)).toEqual(cookieValue)
       })
+      expect(fetchMock).toHaveBeenCalledOnceWith(URLS.AUTH)
     })
 
     it('throws error if cookies are not found', async () => {
       fetchMock.once('', { headers: {} })
-      jest
-        .spyOn(Dune.prototype as any, 'getCsrfToken')
-        .mockImplementationOnce(voidFn)
-
       await expect(dune['getAuthCookies']()).rejects.toMatchInlineSnapshot(
         `[Error: No cookies found on response]`,
       )
     })
   })
 
-  it('getAuthToken', async () => {
-    const TOKEN = 'foo'
-    fetchMock.once(JSON.stringify({ token: TOKEN }))
+  describe('getAuthToken', () => {
+    it('sets token', async () => {
+      fetchMock.once(JSON.stringify({ token: TOKEN }))
 
-    await dune['getAuthToken']()
+      await dune['getAuthToken']()
 
-    expect(dune).toHaveProperty('token', TOKEN)
+      expect(fetchMock).toHaveBeenCalledOnceWith(URLS.SESSION)
+      expect(dune).toHaveProperty('token', TOKEN)
+    })
+  })
+
+  describe('login', () => {
+    it('logs in', async () => {
+      fetchMock
+        .once('', { headers: { 'set-cookie': `csrf=1234` } })
+        .once('', {
+          headers: {
+            'set-cookie': Object.entries(COOKIES)
+              .map(([k, v]) => `${k}=${v}`)
+              .join(';'),
+          },
+        })
+        .once(JSON.stringify({ token: TOKEN }))
+
+      await dune.login()
+      ;[URLS.CSRF, URLS.AUTH, URLS.SESSION].forEach((url, index) => {
+        expect(fetchMock).toHaveBeenNthCalledWith(
+          index + 1,
+          url,
+          expect.any(Object),
+        )
+      })
+      expect(dune).toHaveProperty('token', TOKEN)
+    })
   })
 })
