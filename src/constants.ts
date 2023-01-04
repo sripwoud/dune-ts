99c016261cede7ec137fae8af152be3e03f0a930
r1oga <38692952+r1oga@users.noreply.github.com>
2023-01-04 17:37:07 +0100

    update gql execution query

--- src/constants.ts
+++ src/constants.ts
@@ -36,7 +36,7 @@ export const GET_EXECUTION_ID_DATA = {
 }
 
 const GET_EXECUTION_GQL =
-  'query GetExecution($execution_id: String!, $query_id: Int!, $parameters: [Parameter!]!) {\n  get_execution(\n    execution_id: $execution_id\n    query_id: $query_id\n    parameters: $parameters\n  ) {\n    execution_queued {\n      execution_id\n      execution_user_id\n      position\n      execution_type\n      created_at\n      __typename\n    }\n    execution_running {\n      execution_id\n      execution_user_id\n      execution_type\n      started_at\n      created_at\n      __typename\n    }\n    execution_succeeded {\n      execution_id\n      runtime_seconds\n      generated_at\n      columns\n      data\n      __typename\n    }\n    execution_failed {\n      execution_id\n      type\n      message\n      metadata {\n        line\n        column\n        hint\n        __typename\n      }\n      runtime_seconds\n      generated_at\n      __typename\n    }\n    __typename\n  }\n}\n'
+  'query GetExecution($execution_id: String!, $query_id: Int!, $parameters: [Parameter!]!) {\n  get_execution(\n    execution_id: $execution_id\n    query_id: $query_id\n    parameters: $parameters\n  ) {\n      execution_succeeded {\n        execution_id\n        columns\n        data\n      }\n    }\n}\n'
 export const QUERY_DATA = {
   operationName: 'GetExecution',
   query: GET_EXECUTION_GQL,
