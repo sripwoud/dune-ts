a9ce36dbf5a3504bb2737b4b57825d8e84f5ce2d
sripwoud <me@sripwoud.xyz>
2023-01-05 11:09:36 +0100

    :rotating_light: Update lint rules

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,6 +5,6 @@ import type { Response as _Response } from 'node-fetch'
 // fetch types aren't in node 18 yet
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924#issuecomment-1236920407
 declare global {
-  declare var fetch: typeof _fetch
+  declare const fetch: typeof _fetch
   declare type Response = _Response
 }
